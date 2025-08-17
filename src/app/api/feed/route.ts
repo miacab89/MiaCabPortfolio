1af546c6230e45d742a36f48ddef6962be2480ee
Mia Cabrera <antemia.kavillari@gmail.com>
2025-08-17 17:19:09 -0500

    res

--- src/app/api/feed/route.ts
+++ src/app/api/feed/route.ts
@@ -19,7 +19,7 @@
 // }
 import { Posts } from "@/app/types";
 
-export async function GET({post}: Posts) {
+export async function GET(res: Response, {post}: Posts) {
     const posts = [{post}];
     const result = new Response(JSON.stringify(posts),{
             status:200,
