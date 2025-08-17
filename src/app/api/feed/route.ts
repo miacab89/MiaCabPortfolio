300b3d8b0fcc6edcc0f47c62b6043a028e2ed6fe
Mia Cabrera <antemia.kavillari@gmail.com>
2025-08-17 02:24:39 -0500

    api route

---
+++ src/app/api/feed/route.ts
@@ -0,0 +1,18 @@
+type Posts = {
+    id: number,
+    author: string,
+    content: string[]
+}
+export async function GET({id, author, content}: Posts) {
+const posts = [
+    {
+    id,
+    author,
+    content
+    },
+];
+    return new Response(JSON.stringify(posts),{
+        status:200,
+        headers:{ "Content-Type": "application/json" }
+    })
+}
