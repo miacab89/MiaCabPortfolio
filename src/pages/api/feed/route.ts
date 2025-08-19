c9a7ddf7b550743585c86c4d3e534f2d62be2f7f
Mia Cabrera <antemia.kavillari@gmail.com>
2025-08-19 01:48:27 -0500

    clean up

--- src/pages/api/feed/route.ts
+++ src/pages/api/feed/route.ts
@@ -1,30 +1,3 @@
-// import { NextApiRequest, NextApiResponse } from "next";
-
-// export function NewsFeed (res: NextApiResponse, req: NextApiRequest) {
-//     const { id } = req.query;
-
-//     if (req.method === 'GET') {
-//         res.status(200).json({ id, message: 'Feed data fetched successfully' });
-//     }
-//     // const url = 'https://api.worldnewsapi.com'
-//     // const apiKey = '';
-
-//     // fetch(url, {
-//     //     method: 'GET',
-//     //     headers: {
-//     //         'x-api-key': apiKey
-//     //     }
-//     // })
-//     // .then(response => {
-//     //     if (!response.ok) {
-//     //         throw new Error(`HTTP error! Status: ${response.status}`);
-//     //     }
-//     //     return response.json();
-//     // })
-//     // .then(data => console.log(data))
-//     // .catch(error => console.error('There was a problem with the fetch operation:', error));
-// }
-
 import type { NextApiRequest, NextApiResponse } from 'next'
  
 type ResponseData = {
