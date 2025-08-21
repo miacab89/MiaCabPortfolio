336da96c09667617a8584f749a863b9a457c14b4
Mia Cabrera <antemia.kavillari@gmail.com>
2025-08-21 17:28:05 -0500

    navbar hydration/main page switch

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import {
     NavigationMenu,
+    NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
 } from "@/app/components/ui/navigation-menu"
@@ -10,18 +11,26 @@ export function Navbar() {
         <div className="text-white">
             <NavigationMenu>
                 <NavigationMenuList>
-                    <NavigationMenuLink>
-                        <Link href="/about">About</Link>
-                    </NavigationMenuLink>
-                    <NavigationMenuLink>
-                        <Link href="/music">Music</Link>
-                    </NavigationMenuLink>
-                    <NavigationMenuLink>
-                        <Link href="/finance">Finance</Link>
-                    </NavigationMenuLink>
-                    <NavigationMenuLink>
-                        <Link href="/weather">Weather</Link>
-                    </NavigationMenuLink>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild>
+                            <Link href="/about">About</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild>
+                            <Link href="/music">Music</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild>
+                            <Link href="/finance">Finance</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild>
+                            <Link href="/weather">Weather</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
                 </NavigationMenuList>
             </NavigationMenu>
         </div>
